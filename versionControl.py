069e51e93eea004945434d67a8e6c34362e52fc7
Christian Mora <christian.mora@ufl.edu>
2023-03-08 19:07:12 -0500

    My encoder program is ready, the decode() function can be added.

--- versionControl.py
+++ versionControl.py
@@ -1,3 +1,74 @@
+# Christian Mora
+
+
+# encode() will take in a string of 8 numbers and return the encoded string where each digit equals itself plus 3.
+def encode(password):
+
+    # creating an empty string as a placeholder for the encoded password.
+    encoded_password = ''
+
+    # The for loop iterates for each digit in the inputted password.
+    for digit in password:
+
+        # encoded_digit converts each string digit into an int so 3 can be added,
+        # modulus 10 ensures that the newly encoded digit still falls between 0-9,
+        # then the digit is converted back to a string.
+        encoded_digit = str((int(digit) + 3) % 10)
+
+        # each encoded digit is added to the encoded_password string
+        encoded_password += encoded_digit
+
+    # returns the new encoded password
+    return encoded_password
+
+
+while __name__ == '__main__':
+
+    # quit_menu will equal 1 when the user wants to quit the program.
+    quit_menu = 0
+
+    # The program will loop until the user wants to quit.
+    while quit_menu != 1:
+
+        # Printing the encoder menu.
+        print('Menu\n'
+              '-------------\n'
+              '1. Encode\n'
+              '2. Decode\n'
+              '3. Quit\n')
+
+        # Prompts the user to select a menu option.
+        menu_input = int(input('Please enter an option: '))
+
+        # This if loop runs if the user selects the menu option 1. Encode.
+        if menu_input == 1:
+
+            # Prompts the user to enter a password to encode.
+            pass_to_encode = input('Please enter your password to encode: ')
+
+            # If the password is not 8 digits, an error message is printed.
+            if len(pass_to_encode) != 8:
+                print('Error, your password should be 8 digits.')
+                continue
+
+            # encoded_pass stores the newly encoded password.
+            encoded_pass = encode(pass_to_encode)
+
+            # Prints a statement to let the user know the password was encoded.
+            print('Your password has been encoded and stored!\n')
+
+        # This elif will run if the user selects the menu option 2. Decode.
+        elif menu_input == 2:
+
+            # Prints the encoded password and the decoded password, which is identical to the original password.
+            print('The encoded password is ', encoded_pass, ', and the original password is ', pass_to_encode, '.\n', sep='')
+
+        # This elif will cause the program to end if the user selects menu option 3.
+        elif menu_input == 3:
+            quit_menu = 1
+            break
+
+    break
 
 
 
